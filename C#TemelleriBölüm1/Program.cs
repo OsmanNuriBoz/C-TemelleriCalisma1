24b8353530000e465ce37a10eb8eb61dc0f96fa5
Microsoft <Microsoft@SWENCYSYSTEM>
2024-12-04 18:03:44 +0300

    kdv hesaplama

--- C#TemelleriBölüm1/Program.cs
+++ C#TemelleriBölüm1/Program.cs
@@ -1,28 +1,38 @@
-﻿Console.WriteLine("1.Sayıyı giriniz :");
-int bırıncısayı = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("2.Sayıyı giriniz :");
-int ıkıncısayı = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("1.İsimi giriniz :");
-string bırıncıısım = Console.ReadLine();
-Console.WriteLine("2.İsimi giriniz :");
-string ıkıncıısım = Console.ReadLine();
+﻿//Console.WriteLine("1.Sayıyı giriniz :");
+//int bırıncısayı = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("2.Sayıyı giriniz :");
+//int ıkıncısayı = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("1.İsimi giriniz :");
+//string bırıncıısım = Console.ReadLine();
+//Console.WriteLine("2.İsimi giriniz :");
+//string ıkıncıısım = Console.ReadLine();
 
-int sayılarıncarpımımetod(int sayi1, int sayi2)
+//int sayılarıncarpımımetod(int sayi1, int sayi2)
+//{
+//    int sayılarıncarpımı = sayi1 * sayi2;
+//    return sayılarıncarpımı;
+//}
+
+//string ısımbırlestırmemetod(string isim1, string isim2)
+//{
+//    string ısımbırlestırme = $"{isim1}_{isim2}";
+//    return ısımbırlestırme;
+//}
+
+//int sayılarıncarpımısonucu = sayılarıncarpımımetod(bırıncısayı, ıkıncısayı);
+//Console.WriteLine($"Girilen sayının çarpımı : {sayılarıncarpımısonucu}");
+
+//string ısımbırlesımısonucu = ısımbırlestırmemetod(bırıncıısım, ıkıncıısım);
+//Console.WriteLine($"Girilen ismin birleşimi : {ısımbırlesımısonucu}");
+
+//Console.ReadLine();
+
+
+float kdvhesaplama(float urunfıyatı)
 {
-    int sayılarıncarpımı = sayi1 * sayi2;
-    return sayılarıncarpımı;
+    float kdv = urunfıyatı * 0.18f;
+    return kdv;
 }
 
-string ısımbırlestırmemetod(string isim1, string isim2)
-{
-    string ısımbırlestırme = $"{isim1}_{isim2}";
-    return ısımbırlestırme;
-}
-
-int sayılarıncarpımısonucu = sayılarıncarpımımetod(bırıncısayı, ıkıncısayı);
-Console.WriteLine($"Girilen sayının çarpımı : {sayılarıncarpımısonucu}");
-
-string ısımbırlesımısonucu = ısımbırlestırmemetod(bırıncıısım, ıkıncıısım);
-Console.WriteLine($"Girilen ismin birleşimi : {ısımbırlesımısonucu}");
-
-Console.ReadLine();
+float kdv = kdvhesaplama(1500f);
+Console.WriteLine(kdv);
